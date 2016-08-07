30f4222ebd657a36f35c1c5bc55dd95562123d00
Nathan Brown <nathan@nkbrown.us>
2016-08-07 13:03:53 -0700

    NH-3853 - Add CoalesceType so projections don't rely on SerializableType.

---
+++ src/NHibernate/Type/CoalesceType.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+
+namespace NHibernate.Type
+{
+	/// <summary>
+	/// Handles any type returned from a Coalesce projection
+	/// </summary>
+	[Serializable]
+	public class CoalesceType : PrimitiveType
+	{
+		public CoalesceType() : base(new SqlTypes.SqlType(DbType.Object))
+		{
+		}
+
+		public override string Name
+		{
+			get { return "Coalesce"; }
+		}
+
+		public override System.Type ReturnedClass
+		{
+			get { return typeof(object); }
+		}
+
+		public override void Set(IDbCommand cmd, object value, int index)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override object Get(IDataReader rs, int index)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override object Get(IDataReader rs, string name)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override object FromStringValue(string xml)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override System.Type PrimitiveClass
+		{
+			get { return typeof(object); }
+		}
+
+		public override object DefaultValue
+		{
+			get { return null; }
+		}
+
+		public override string ObjectToSQLString(object value, Dialect.Dialect dialect)
+		{
+			return value.ToString();
+		}
+	}
+}
