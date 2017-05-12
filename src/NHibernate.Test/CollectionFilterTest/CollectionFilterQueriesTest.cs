15a462ce93b27e71c1b963dfa1ff03276183819a
Nathan Brown <nathan@nkbrown.us>
2017-05-12 00:05:00 -0700

    NH-3807 - revert commit 5c448337500970489009feb2c2e87a59ecdd4b31
    Merge pull request #551 from hazzik/NH-3945
    
    NH-3945 - Update Antlr3 to v 3.5.1

--- src/NHibernate.Test/CollectionFilterTest/CollectionFilterQueriesTest.cs
+++ src/NHibernate.Test/CollectionFilterTest/CollectionFilterQueriesTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Antlr.Runtime;
 using NHibernate.Hql.Ast.ANTLR;
 using NUnit.Framework;
 using System.Collections;
